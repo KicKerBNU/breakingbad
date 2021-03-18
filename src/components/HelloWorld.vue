8c9ed2331cad1b21bf25bd2f0ccd964828b88945
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2021-03-18 00:26:05 -0300

    Update layout breaking bad

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container center-align">
+    <div id="menu-id" class="menu top medium-device large-device">
+      <div class="small-space"></div>
+      <img src="../assets/icon.png" class="front small no-margin no-padding" />
+      <img src="../assets/bb-gif.gif" class="front large no-margin no-padding" />
+      <a>
+        <img src="../assets/characters.png" class="front tiny no-margin no-padding" />
+        <div>Characters</div>
+      </a>
+      <a>
+        <img src="../assets/episodes.png" class="front tiny no-margin no-padding" />
+        <div>Episodes</div>
+      </a>
+      <a>
+        <img src="../assets/quotes.png" class="front tiny no-margin no-padding" />
+        <div>Quotes</div>
+      </a>
+      <a>
+        <img src="../assets/deaths.png" class="front tiny no-margin no-padding" />
+        <div>Deaths</div>
+      </a>
+      <img src="../assets/bb-gif.gif" class="front large no-margin no-padding" />
+      <img src="../assets/icon.png" class="front small no-margin no-padding" />
+      
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
 </style>
